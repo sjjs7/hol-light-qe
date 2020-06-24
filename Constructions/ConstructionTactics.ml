6e36919e83dd9fef39553c99d17f9ae74737f77e
Johanna <64925556+sjjs7@users.noreply.github.com>
2020-06-24 18:25:09 -0400

    Update ConstructionTactics.ml

--- Constructions/ConstructionTactics.ml
+++ Constructions/ConstructionTactics.ml
@@ -1,3 +1,3 @@
 (*Two tactics to make use of quotation easier inside proofs*)
 let (UNQUOTE_TAC: tactic) = fun (asm,gl) -> PURE_REWRITE_TAC[UNQUOTE_CONV gl] (asm,gl);;
-let (TERM_TO_CONSTRUCTION_TAC: tactic) = fun (asm,gl) -> PURE_REWRITE_TAC[TERM_TO_CONSTRUCTION_CONV gl] (asm,gl);;
+let (QUOTE_TO_CONSTRUCTION_TAC: tactic) = fun (asm,gl) -> PURE_REWRITE_TAC[QUOTE_TO_CONSTRUCTION_CONV gl] (asm,gl);;
