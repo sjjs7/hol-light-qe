b0c981a1b134ccbf9c28154af5dfda29ce3388b3
Johanna <64925556+sjjs7@users.noreply.github.com>
2020-07-02 16:18:08 -0400

    Update equal.ml

--- equal.ml
+++ equal.ml
@@ -237,7 +237,7 @@ let (ONCE_DEPTH_CONV: conv->conv),
 
     match tm with 
     | Abs(_,_) -> ABS_CONV conv tm
-    | Quote(_,_) -> QSUB_CONV conv tm (TDQR)
+    | Quote(_) -> QSUB_CONV conv tm (TDQR)
     | Eval(_,_) -> QSUB_CONV conv tm (TDQR)
     | _ -> COMB_QCONV conv tm in
 
