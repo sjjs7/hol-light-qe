8d684b25571fe07d7276dc67fa2f14b4b157ec0d
Johanna <64925556+sjjs7@users.noreply.github.com>
2020-07-19 19:43:41 -0400

    Update LEM_Example.ml

--- Constructions/LEM_Example.ml
+++ Constructions/LEM_Example.ml
@@ -1,4 +1,4 @@
-needs "Constructions/epsilon.ml";;
+needs "Constructions/QuotationTactics.ml";;
 (* Law of excluded middle (LEM) *)
 
 let lem = prove(`!x:epsilon. isExprType x (TyBase "bool") ==> ((eval x to bool) \/ ~(eval x to bool))`,
@@ -32,15 +32,10 @@ INST [`QuoConst "T" (TyBase "bool")`,`x:epsilon`] lem;;
 
 	(*FIRST ANTECEDENT: isExprType isExprType (QuoConst "T" (TyBase "bool")) (TyBase "bool")*)
 	let inst_isExpr = prove(`isExprType (QuoConst "T" (TyBase "bool")) (TyBase "bool")`,
-		REWRITE_TAC[isExprType] THEN
-		REWRITE_TAC[isExpr] THEN
-		REWRITE_TAC[isValidConstName] THEN
-		REWRITE_TAC[isValidType] THEN
-		REWRITE_TAC[EX] THEN
-		REWRITE_TAC[combinatoryType] (*INST [`"T"`,`str:string`;`(TyBase "bool")`,`ty:type`] (CONJUNCT1 (CONJUNCT2 combinatoryType));;*)
-	);;
+		IS_EXPR_TYPE_TAC);;
+
 	(*SECOND ANTECEDENT: ~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "T" (TyBase "bool"))*)
-	let notIsFree = prove(`~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "T" (TyBase "bool"))`,REWRITE_TAC[isFreeIn]);;
+	let notIsFree = prove(`~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "T" (TyBase "bool"))`,IS_FREE_IN_TAC);;
 
 	(*Now we can easily prove the relationship between the abstracted version of the eval and the eval with QuoConst inside*)
 
@@ -64,16 +59,12 @@ INST [`QuoConst "T" (TyBase "bool")`,`x:epsilon`] lem;;
 	);;
 
 	let true_LEM = prove(`(eval (QuoConst "T" (TyBase "bool")) to (bool)) \/ ~(eval (QuoConst "T" (TyBase "bool")) to (bool))`,
-	REWRITE_TAC[GSYM (MP (EQ_MP (ONCE_DEPTH_CONV BETA_CONV (concl (BETA_REDUCE_EVAL `x:epsilon` `(QuoConst "T" (TyBase "bool"))` `x:epsilon` `:bool`))) (BETA_REDUCE_EVAL `x:epsilon` `(QuoConst "T" (TyBase "bool"))` `x:epsilon` `:bool`)) (MESON[CONJ ((REWRITE_CONV[isExprType;isExpr;isValidConstName;isValidType;EX;combinatoryType]) `isExprType (QuoConst "T" (TyBase "bool")) (TyBase "bool")`) ((REWRITE_CONV[isFreeIn]) `~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "T" (TyBase "bool"))`)] `isExprType (QuoConst "T" (TyBase "bool")) (TyBase "bool") /\ ~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "T" (TyBase "bool"))`))] THEN
-	REWRITE_TAC [(MP (SPEC `QuoConst "T" (TyBase "bool")` lem) (MESON[((REWRITE_CONV[isExprType;isExpr;isValidConstName;isValidType;EX;combinatoryType]) `isExprType (QuoConst "T" (TyBase "bool")) (TyBase "bool")`)] `isExprType (QuoConst "T" (TyBase "bool")) (TyBase "bool")`))]
-	);;
+	REWRITE_TAC[EXCLUDED_MIDDLE]);;
 
 INST [`QuoConst "F" (TyBase "bool")`,`x:epsilon`] lem;;
 
 	let false_LEM = prove(`(eval (QuoConst "F" (TyBase "bool")) to (bool)) \/ ~(eval (QuoConst "F" (TyBase "bool")) to (bool))`,
-	REWRITE_TAC[GSYM (MP (EQ_MP (ONCE_DEPTH_CONV BETA_CONV (concl (BETA_REDUCE_EVAL `x:epsilon` `(QuoConst "F" (TyBase "bool"))` `x:epsilon` `:bool`))) (BETA_REDUCE_EVAL `x:epsilon` `(QuoConst "F" (TyBase "bool"))` `x:epsilon` `:bool`)) (MESON[CONJ ((REWRITE_CONV[isExprType;isExpr;isValidConstName;isValidType;EX;combinatoryType]) `isExprType (QuoConst "F" (TyBase "bool")) (TyBase "bool")`) ((REWRITE_CONV[isFreeIn]) `~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "F" (TyBase "bool"))`)] `isExprType (QuoConst "F" (TyBase "bool")) (TyBase "bool") /\ ~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "F" (TyBase "bool"))`))] THEN
-	REWRITE_TAC [(MP (SPEC `QuoConst "F" (TyBase "bool")` lem) (MESON[((REWRITE_CONV[isExprType;isExpr;isValidConstName;isValidType;EX;combinatoryType]) `isExprType (QuoConst "F" (TyBase "bool")) (TyBase "bool")`)] `isExprType (QuoConst "F" (TyBase "bool")) (TyBase "bool")`))]
-	);;
+	REWRITE_TAC[EXCLUDED_MIDDLE]);;
 
 INST [`Q_ (0 = 0) _Q`,`x:epsilon`] lem;;
 
@@ -90,21 +81,7 @@ INST [`Q_ (0 = 0) _Q`,`x:epsilon`] lem;;
 
 	(*FIRST ANTECEDENT: isExprType isExprType construction_form*)
 	let inst_isExpr_quote = prove(`isExprType (App (App (QuoConst "=" (TyBiCons "fun" (TyBase "num") (TyBiCons "fun" (TyBase "num") (TyBase "bool")))) (App (QuoConst "NUMERAL" (TyBiCons "fun" (TyBase "num") (TyBase "num"))) (QuoConst "_0" (TyBase "num")))) (App (QuoConst "NUMERAL" (TyBiCons "fun" (TyBase "num") (TyBase "num"))) (QuoConst "_0" (TyBase "num")))) (TyBase "bool")`,
-		REWRITE_TAC[isExprType] THEN
-		REWRITE_TAC[isExpr] THEN
-		REWRITE_TAC[combinatoryType] THEN
-		REWRITE_TAC[stripFunc] THEN
-		REWRITE_TAC[headFunc] THEN
-		REWRITE_TAC[isFunction;isAbs;isApp;isConst] THEN
-		REWRITE_TAC[isValidType;isValidConstName] THEN
-		REWRITE_TAC[EX;ep_constructor] THEN
-		REPEAT CONJ_TAC THENL [
-			EXISTS_TAC `TyBase "num"` THEN EXISTS_TAC `TyBase "bool"` THEN REFL_TAC;
-			EXISTS_TAC `TyBase "num"` THEN EXISTS_TAC `TyBase "num"` THEN REFL_TAC; 
-			EXISTS_TAC `TyBase "num"` THEN EXISTS_TAC `TyBiCons "fun" (TyBase "num") (TyBase "bool")` THEN REFL_TAC;
-			EXISTS_TAC `TyBase "num"` THEN EXISTS_TAC `TyBase "num"` THEN REFL_TAC
-			]
-	);;
+		IS_EXPR_TYPE_TAC);;
 
 	(*SECOND ANTECEDENT: ~isFreeIn (QuoVar "x" (TyBase "epsilon")) (QuoConst "T" (TyBase "bool"))*)
 	let notIsFree_quote = prove(`~isFreeIn (QuoVar "x" (TyBase "epsilon")) (App (App (QuoConst "=" (TyBiCons "fun" (TyBase "num") (TyBiCons "fun" (TyBase "num") (TyBase "bool")))) (App (QuoConst "NUMERAL" (TyBiCons "fun" (TyBase "num") (TyBase "num"))) (QuoConst "_0" (TyBase "num")))) (App (QuoConst "NUMERAL" (TyBiCons "fun" (TyBase "num") (TyBase "num"))) (QuoConst "_0" (TyBase "num"))))`,
@@ -136,7 +113,6 @@ INST [`Q_ (0 = 0) _Q`,`x:epsilon`] lem;;
 		REWRITE_TAC[quote_construct_equiv] THEN
 		REWRITE_TAC[quoteLEM]
 	);;
-
 
 
 
